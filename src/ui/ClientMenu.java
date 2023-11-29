fb4b9dbf23ff5eb36f0ed3b6eba2bb9706b90346
luisgdj <luisgdj02@gmail.com>
2023-11-29 16:22:47 +0100

    .

--- src/ui/ClientMenu.java
+++ src/ui/ClientMenu.java
@@ -6,39 +6,45 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import threads.ClientTCP;
-import threads.ServerTCP;
 
 public abstract class ClientMenu {
 
 	public static void menu(String email) {
 
+		File file = null;
 		while (true) {
-			System.out.println("\nEMG signal (patient menu: " + email + ")" 
-					+ "\n 1. View signal"
-					+ "\n 2. _____________" 
+			//ALS: Amyotrophic Lateral Sclerosis (ELA)
+			System.out.println("\nPatient ALS diagnostic menu: (patient: " + email + ")" 
+					+ "\n 1. Register new data"
+					+ "\n 2. Send data to server"
+					+ "\n 3. Check doctor diagnostic"
+					+ "\n 4. Change password"
 					+ "\n 0. Log out");
 			int option = Utilities.readInteger("Choose an option: ");
 
 			switch (option) {
 				case 1: {
-					
+					//introducir sintomas
+					//grabar señal de bitalino
+					file = new File("misc\\FileToRead.txt");
+					//guardar datos en un fichero (variable file)
 					break;
 				}
 				case 2: {
-					//Sent file to server:
-					String serverIP = "10.60.85.53";
-					int port = 9000;
-					try {
-						Socket socket = new Socket(serverIP, port);
-					    File file = new File("misc\\FileToRead.txt");
-					    ClientTCP server = new ClientTCP(socket, file);
-					    
-					} catch (UnknownHostException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
+					//enviar datos (file) al servidor
+					if(file != null) {
+						sendDataToServer();
+					}else {
+						System.out.println("ERROR: No data has been recorded yet.");
 					}
-					
+					break;
+				}
+				case 3: {
+					//recibir datos del servidor
+					break;
+				}
+				case 4: {
+					//cambiar contraseña
 					break;
 				}
 				case 0: {
@@ -49,6 +55,23 @@ public abstract class ClientMenu {
 					System.out.println(" ERROR: Invalid option.");
 				}
 			}
+		}
+	}
+
+	//OPTION 
+	private static void sendDataToServer() {
+		//Sent file to server:
+		String serverIP = "10.60.85.53";
+		int port = 9000;
+		try {
+			Socket socket = new Socket(serverIP, port);
+		    File file = new File("misc\\FileToRead.txt");
+		    ClientTCP server = new ClientTCP(socket, file);
+		    
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/ui/ServerMenu.java
+++ src/ui/ServerMenu.java
@@ -3,6 +3,8 @@ package ui;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import threads.ServerTCP;
 
@@ -10,50 +12,54 @@ public abstract class ServerMenu {
 
 	public static void menu() {
 
-		while (true) {
-		    try {
-		    	ServerSocket serverSocket = new ServerSocket(9000);
-		        while (true) {
-		            //Thie executes when we have a client
-		            Socket socket;
-					socket = serverSocket.accept();
-		            new Thread(new ServerTCP(socket)).start();
-		            System.out.println("Sever conection stablished!");
-		        }
-		    } catch (IOException e) {
-				e.printStackTrace();
+		try {
+	    	ServerSocket serverSocket = new ServerSocket(9000);
+	        while (true) {
+	            //This executes when we have a client
+	            Socket socket;
+				socket = serverSocket.accept();
+	            new Thread(new ServerTCP(socket)).start();
+	            
+	            System.out.println("Sever conection stablished!");
+	        	while (true) {
+	        		
+					System.out.println("\nHospital ALS diagnostic menu (EMG signal):" 
+							+ "\n 1. Register new patient"
+							+ "\n 2. View patient information"
+							+ "\n 3. Send patient diagnostic"
+							+ "\n 4. Change password"
+							+ "\n 0. Log out");
+					int option = Utilities.readInteger("Choose an option: ");
+		
+					switch (option) {
+						case 1: {
+							//registrar un nuevo paciente
+							break;
+						}
+						case 2: {
+							//coger el fichero de informacion enviado por el paciente y visualizarlo
+							break;
+						}
+						case 3: {
+							//analizar fichero recibido y enviar diagnostico al paciente
+							break;
+						}
+						case 4: {
+							//cambiar contraseña
+						}
+						case 0: {
+							System.out.println("Program terminated.");
+							serverSocket.close();
+							return;
+						}
+						default: {
+							System.out.println(" ERROR: Invalid option.");
+						}
+					}
+	        	}
 			}
-		    
-		        
-		    
-			System.out.println("\nHospital menu (EMG signal):" 
-					+ "\n 1. Register patient"
-					+ "\n 2. Change password"
-					+ "\n 3. ____________"
-					+ "\n 0. Log out");
-			int option = Utilities.readInteger("Choose an option: ");
-
-			switch (option) {
-				case 1: {
-					
-					break;
-				}
-				case 2: {
-					
-					break;
-				}
-				case 3: {
-					
-					break;
-				}
-				case 0: {
-					System.out.println("Program terminated.");
-					return;
-				}
-				default: {
-					System.out.println(" ERROR: Invalid option.");
-				}
-			}
+		} catch (IOException e) {
+			Logger.getLogger(ServerTCP.class.getName()).log(Level.SEVERE, null, e);
 		}
 	}
 }
